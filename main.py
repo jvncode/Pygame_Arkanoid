869b2c1b61f6c3158fa5e4237e6279a5c86349c5
JVNCODE <jvn@MacBook-Pro-de-JESUS.local>
2020-01-21 23:46:15 +0100

    Creación y presentación de raqueta

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pygame as pg
 from pygame.locals import *
 import sys
 
+from entities import *
+
+
 FPS = 60
 
 class Game:
@@ -13,6 +16,10 @@ class Game:
         pg.display.set_caption('Mi Arkanoid')
 
         self.background_img = pg.image.load('resources/background.png').convert()
+        self.player = Racket()
+
+        self.allSprites = pg.sprite.Group()
+        self.allSprites.add(self.player)
 
     def gameOver(self):
         pg.quit()
@@ -29,6 +36,9 @@ class Game:
 
             self.handleEvents()
             self.screen.blit(self.background_img, (0,0))
+
+            self.allSprites.update(dt)
+            self.allSprites.draw(self.screen)
 
             pg.display.flip()
 
